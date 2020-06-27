6a8174a001cd74a20c64b54aca0f4bf9c4ff0471
Hank <hankleo92@gmail.com>
2020-06-27 19:21:11 +0800

    create entities with JDL Studio

--- src/main/java/me/hank/bugtracker/config/CacheConfiguration.java
+++ src/main/java/me/hank/bugtracker/config/CacheConfiguration.java
@@ -51,6 +51,11 @@ public class CacheConfiguration {
             createCache(cm, me.hank.bugtracker.domain.User.class.getName());
             createCache(cm, me.hank.bugtracker.domain.Authority.class.getName());
             createCache(cm, me.hank.bugtracker.domain.User.class.getName() + ".authorities");
+            createCache(cm, me.hank.bugtracker.domain.Project.class.getName());
+            createCache(cm, me.hank.bugtracker.domain.Label.class.getName());
+            createCache(cm, me.hank.bugtracker.domain.Label.class.getName() + ".tickets");
+            createCache(cm, me.hank.bugtracker.domain.Ticket.class.getName());
+            createCache(cm, me.hank.bugtracker.domain.Ticket.class.getName() + ".labels");
             // jhipster-needle-ehcache-add-entry
         };
     }
